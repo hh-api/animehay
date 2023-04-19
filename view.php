7cd5aaf2bcc913188e25c4ee8e17215276850f61
hh-api <131185043+hh-api@users.noreply.github.com>
2023-04-19 14:52:20 +0700

    Create view.php

---
+++ view.php
@@ -0,0 +1,145 @@
+<?php
+include 'includes/header.php';
+header('Cache-Control: max-age=300');
+$slug = $_GET['phim'];
+$movie = './movie/'.$slug.'.php';
+$list = './list/'.$slug.'.php';
+if (file_exists($movie)) {
+include $movie;
+
+if (time() > ($time + 600)) {
+$html = file_get_contents('https://api-hh.blogspot.com/2023/04/'.$slug.'.html');
+$info = explode('</div>', explode('<div class="home">', $html)['1'])['0'];
+$thumb = explode('"', explode('src="', $info)['1'])['0'];
+$noidung = explode('</td>', explode('max-width:1px;">', $info)['1'])['0'];
+$phim = explode("<td>", $info);
+$tenphim = explode("</td>", $phim['2'])['0'];
+$tengoc = explode("</td>", $phim['3'])['0'];
+$stt = explode("</td>", $phim['4'])['0'];
+$nam = explode("</td>", $phim['5'])['0'];
+$hd = explode("</td>", $phim['6'])['0'];
+$quocgia = explode("</td>", $phim['7'])['0'];
+
+$nd = '<?php $time="'.time().'"; $tenphim="'.$tenphim.'"; $tengoc="'.$tengoc.'"; $noidung="'.$noidung.'"; $quocgia="'.$quocgia.'"; $stt="'.$stt.'"; $nam="'.$nam.'"; $hd="'.$hd.'"; $thumb="'.$thumb.'"; ?>';
+$myfile = fopen($movie, "w");
+fwrite($myfile, $nd);
+fclose($myfile);
+include $option;
+
+$list0 = explode('</div>', explode('<div class="list">', $html)['1'])['0'];
+$list0 = preg_replace('/\R+/', "\n", trim($list0));
+$myfile1 = fopen($list, "w");
+fwrite($myfile1, $list0);
+fclose($myfile1);    
+}
+
+} else {
+$html = file_get_contents('https://api-hh.blogspot.com/2023/04/'.$slug.'.html');
+$info = explode('</div>', explode('<div class="home">', $html)['1'])['0'];
+$thumb = explode('"', explode('src="', $info)['1'])['0'];
+$noidung = explode('</td>', explode('max-width:1px;">', $info)['1'])['0'];
+$phim = explode("<td>", $info);
+$tenphim = explode("</td>", $phim['2'])['0'];
+$tengoc = explode("</td>", $phim['3'])['0'];
+$stt = explode("</td>", $phim['4'])['0'];
+$nam = explode("</td>", $phim['5'])['0'];
+$hd = explode("</td>", $phim['6'])['0'];
+$quocgia = explode("</td>", $phim['7'])['0'];
+
+$nd = '<?php $time="'.time().'"; $tenphim="'.$tenphim.'"; $tengoc="'.$tengoc.'"; $noidung="'.$noidung.'"; $quocgia="'.$quocgia.'"; $stt="'.$stt.'"; $nam="'.$nam.'"; $hd="'.$hd.'"; $thumb="'.$thumb.'"; ?>';
+$myfile = fopen($movie, "w");
+fwrite($myfile, $nd);
+fclose($myfile);
+include $option;
+
+$list0 = explode('</div>', explode('<div class="list">', $html)['1'])['0'];
+$list0 = preg_replace('/\R+/', "\n", trim($list0));
+$myfile1 = fopen($list, "w");
+fwrite($myfile1, $list0);
+fclose($myfile1);
+
+}
+?>
+<div class="ah_content">
+<div class="info-movie">
+
+    <h1 class="heading_movie"><font color="yellow"><?php echo $tenphim; ?></font></h1>
+    <div class="head ah-frame-bg">
+        <div class="first">
+            <img src="<?php echo $thumb; ?>" alt="<?php echo $tenphim; ?>" />
+        </div>
+        <div class="last">
+                            <div class="name_other">
+                    <div>Tên Khác</div>
+                    <div><?php echo $tengoc; ?></div>
+                </div>
+            <div class="status">
+                <div>Trạng Thái</div>
+                <div>Tập <?php echo $stt; ?></div>
+            </div>    
+            <div class="list_cate">
+                <div>Thể Loại</div>
+                <div>Updating</div>
+            </div>
+            <div class="score">
+                <div>Điểm</div>
+                <div>
+                    9.1 || 1906 đánh giá                </div>
+            </div>
+            <div class="update_time">
+                <div>Phát Hành</div>
+                <div><?php echo $nam; ?></div>
+            </div>
+            <div class="duration">
+                <div>Số Tập</div>
+                <div>N/A</div>
+            </div>
+        </div>
+    </div>
+    <div class="flex ah-frame-bg flex-wrap">
+        <div class="flex flex-wrap flex-1">
+<?php
+$list0 = file_get_contents($list);
+$first = explode('|', $list0)['0'];
+?>            
+            <a href="<?php echo '/'.$first.'/'.$slug; ?>.html" class="padding-5-15 fs-35 button-default fw-500 fs-15 flex flex-hozi-center bg-lochinvar" title="Xem Ngay"><span class="material-icons-round">play_circle_outline</span></a>
+            <a href="javascript:void(0)" id="toggle_follow" class="bg-green padding-5-15 fs-35 button-default fw-500 fs-15 flex flex-hozi-center" title="Theo dõi phim này"><span class="material-icons-round">bookmark_add</span></a>
+        </div>
+    </div>
+
+        <div class="body">
+        <div class="list_episode ah-frame-bg">
+            <div class="heading flex flex-space-auto fw-700">
+                <span>Danh Sách Tập</span>
+                <span id="newest-ep-is-readed" class="fs-13"></span>
+            </div>
+            <div class="list-item-episode scroll-bar">
+<?php
+$list1 = preg_replace('~\\R~u', "\n", $list0);
+$list1 = explode("\n", $list1);
+foreach ($list1 as $list1) {
+if (strpos($list1, '|') == true) {    
+$get_tap = explode("|", $list1)['0'];    
+?>                
+<a href="<?php echo '/'.$get_tap.'/'.$slug; ?>.html" title="Tập <?php echo $get_tap; ?>"><span><?php echo $get_tap; ?></span></a>
+<?php } } ?>
+            </div>
+        </div>
+        <div class="desc ah-frame-bg">
+            <div>
+                <h2 class="heading">
+                    Nội Dung
+                </h2>
+            </div>
+            <?php echo $noidung; ?>
+        </div>
+    </div>
+
+
+</div>
+
+
+
+<?php
+include 'includes/footer.php';
+?>
