1cfffe418e79c062f0334df7aa48ccde36ea451e
hh-api <131185043+hh-api@users.noreply.github.com>
2023-04-19 16:52:19 +0700

    Create footer.php

---
+++ includes/footer.php
@@ -0,0 +1,7 @@
+<div class="flex flex-ver-center fw-500">
+<a target="_blank" href="https://t.me/+fRK6LPYsUl1jYzU1"><img width="50px" src="https://lh3.googleusercontent.com/-VGdNCwWEI6Y/ZD7vPphlzeI/AAAAAAAADBA/Y40IlX_arRQYleYQ_JW3BzFk2M-f__fCACNcBGAsYHQ/s0/tele.png"/></a>    
+</div>
+</div>
+</div>
+</body>
+</html>
